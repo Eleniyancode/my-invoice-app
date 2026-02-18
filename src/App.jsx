82a62d11b56dac6320b7c59b9edfab6214acbe33
Abdulrahaman Ridwan Ayinla <abdulrahamanr657@gmail.com>
2026-02-18 10:44:11 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1 className="bg-primary">Hello World!</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#7C5DFA",
+        secondary: "#9277FF",
+        background: "#F8F8FB",
+        dark: "#141625",
+        paid: "#33D69F",
+        pending: "#FF8F00",
+        draft: "#373B53",
+      },
+    },
+  },
+  plugins: [],
+};
